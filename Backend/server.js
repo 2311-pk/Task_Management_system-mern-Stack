5a8623e9cedc287c98680a4daf5a0e59d1af737c
Prathmesh kulkarni <prathmeshkulkarni312@gmail.com>
2025-01-29 22:37:16 +0530

    fourth

--- Backend/server.js
+++ Backend/server.js
@@ -7,7 +7,7 @@ const T =require("./Routes/TaskRoutes");
 const { default: mongoose } = require("mongoose");
 const allowedOrigins = [
     'http://localhost:5173', // Local development
-    'https://mern-stack-hospital-food-management-system-frontend.vercel.app' // Deployed frontend
+    'https://task-management-system-mern-stack-w9va-k2r92lkjc.vercel.app' // Deployed frontend
   ];
   
 require("dotenv").config();

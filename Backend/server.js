1d3152b9ba0e7fb71d9fb84f97a3c0b8fa8ef058
Prathmesh kulkarni <prathmeshkulkarni312@gmail.com>
2025-01-29 22:41:19 +0530

    fifth

--- Backend/server.js
+++ Backend/server.js
@@ -7,12 +7,23 @@ const T =require("./Routes/TaskRoutes");
 const { default: mongoose } = require("mongoose");
 const allowedOrigins = [
     'http://localhost:5173', // Local development
-    'https://task-management-system-mern-stack-w9va-k2r92lkjc.vercel.app' // Deployed frontend
+    'https://task-management-system-mern-stack-w9va.vercel.app/' // Deployed frontend
   ];
   
 require("dotenv").config();
 const app=express();
-app.use(cors());
+app.use(
+    cors({
+      origin: function (origin, callback) {
+        if (allowedOrigins.includes(origin) || !origin) {
+          callback(null, true);
+        } else {
+          callback(new Error('Not allowed by CORS'));
+        }
+      },
+      credentials: true // Allow cookies if needed
+    })
+  );
 app.use(express.json());
 app.use("/api/TaskRoutes",T);
 mongoose.connect(process.env.MONGO_URI)

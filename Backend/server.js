e9f42a04eb5405cd754c7cb0a26b401fac8905e9
Prathmesh kulkarni <prathmeshkulkarni312@gmail.com>
2025-01-29 23:16:36 +0530

    nine

--- Backend/server.js
+++ Backend/server.js
@@ -5,10 +5,6 @@ const cors = require("cors");
 const Task = require("./models/Task");
 const T =require("./Routes/TaskRoutes");
 const { default: mongoose } = require("mongoose");
-const allowedOrigins = [
-    'http://localhost:5173', // Local development
-    'https://task-management-system-mern-stack-w9va.vercel.app' // Deployed frontend
-  ];
   
 require("dotenv").config();
 const app=express();

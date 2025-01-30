43317292b4689cb7d6d533f69f8f144d3a8c5ac7
Prathmesh kulkarni <prathmeshkulkarni312@gmail.com>
2025-01-30 11:23:17 +0530

    Updated Dashboard.jsx

--- Frontend/src/pages/Dashboard.jsx
+++ Frontend/src/pages/Dashboard.jsx
@@ -37,27 +37,29 @@ function Dashboard()
   },[]);
 
     return(<>
+    <div className="w-screen h-screen bg-gradient-to-br from-cyan-900 to-cyan-300">
+    <div
+ className="w-full max-w-screen-xl mx-auto ">
 <h1>Dashboard page</h1>
 <h1>username :{username}</h1>
 <div className="flex flex-wrap gap-4 justify-center sm:justify-between mx-8">
 
-<div className="w-36 border-2 border-cyan-200 p-3 lg:w-64 ">
-<h1>Total Task</h1>
+<div className="w-36 border-4 border-white p-3 lg:w-64  rounded-4xl bg-purple-200">
+<h1 className="mx-auto w-32 text-2xl font-semibold text-black">Total Task</h1>
 </div>
-<div className="w-36 border-2 border-cyan-200 p-3 lg:w-64 ">
-<h1>Total Task</h1>
+<div className="w-40 border-4 border-white p-3 lg:w-64  rounded-4xl bg-purple-200">
+<h1 className="mx-auto w-32 text-xl font-semibold text-black">Pending Task</h1>
 </div>
-<div className="w-36 border-2 border-cyan-200 p-3 lg:w-64 ">
-<h1>Total Task</h1>
+<div className="w-56 border-4 border-white pt-3 pb-3 lg:w-64  rounded-4xl bg-purple-200">
+<h1 className="mx-auto w-40 text-xl font-semibold text-black">Completed Task</h1>
 </div>
-<div className="w-36 border-2 border-cyan-200 p-3 lg:w-64 ">
-<h1>Total Task</h1>
-</div>
+
+
 </div>
 <div>
     
-<table className="text-wrap">
-  <thead><tr>
+<table className="text-wrap  w-full mt-5">
+  <thead ><tr className="border-2 border-cyan-300 p-3">
 <th>taskname</th>
 <th>description</th>
 <th>dueDate</th>
@@ -76,6 +78,7 @@ key={t._id}>
   </tbody>
 </table>
 
+</div></div>
 </div>
 </>
 

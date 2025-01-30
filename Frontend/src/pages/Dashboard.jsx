f55a2b899bdeca968dcdd78c15ce00378d92b7c9
Prathmesh kulkarni <prathmeshkulkarni312@gmail.com>
2025-01-30 12:06:20 +0530

    Updated3 Dashboard.jsx

--- Frontend/src/pages/Dashboard.jsx
+++ Frontend/src/pages/Dashboard.jsx
@@ -37,31 +37,31 @@ function Dashboard()
   },[]);
 
     return(<>
-    <div className="w-screen h-screen bg-gradient-to-br from-cyan-900 to-cyan-300">
+    <div className="w-screen h-screen bg-gradient-to-br from-gray-900 to-gray-700">
     <div
  className="w-full max-w-screen-xl mx-auto ">
 <h1>Dashboard page</h1>
 <h1>username :{username}</h1>
 <div className="flex flex-wrap gap-4 justify-center sm:justify-between mx-8">
 
-<div className="w-36 border-4 border-white p-3 lg:w-64  rounded-4xl bg-purple-200">
-<h1 className="mx-auto w-32 text-2xl font-semibold text-black">Total Task</h1>
+<div className="shadow-2xl shadow-white w-36 border-4 border-white p-3 lg:w-64  rounded-4xl bg-purple-200">
+<h1 className="mx-auto w-32 text-xl font-semibold text-black">Total Task</h1>
 </div>
-<div className="w-40 border-4 border-white p-3 lg:w-64  rounded-4xl bg-purple-200">
+<div className=" shadow-2xl shadow-white w-40 border-4 border-white p-3 lg:w-64  rounded-4xl bg-purple-200">
 <h1 className="mx-auto w-32 text-xl font-semibold text-black">Pending Task</h1>
 </div>
-<div className="w-56 border-4 border-white pt-3 pb-3 lg:w-64  rounded-4xl bg-purple-200">
+<div className="shadow-2xl shadow-white  w-56 border-4 border-white pt-3 pb-3 lg:w-64  rounded-4xl bg-purple-200">
 <h1 className="mx-auto w-40 text-xl font-semibold text-black">Completed Task</h1>
 </div>
 
 
 </div>
-<div>
+<div className="bg-white mt-8 rounded-4xl border-4 border-white  shadow-2xl   shadow-white">
     
-<div className="overflow-x-auto">
-  <table className="text-wrap w-full mt-5">
+<div className="overflow-x-auto rounded-4xl p-2 bg-indigo-300">
+  <table className="text-wrap w-full rounded-4xl">
     <thead>
-      <tr className="border-2 border-cyan-300 p-3">
+      <tr className=" p-3">
         <th>Task Name</th>
         <th>Description</th>
         <th>Due Date</th>
@@ -71,10 +71,10 @@ function Dashboard()
     <tbody>
       {task.map((t) => (
         <tr className="border-2 w-full text-center border-blue-300" key={t._id}>
-          <td className="border-2 border-cyan-300 p-3">{t.name}</td>
-          <td className="border-2 border-cyan-300 p-3">{t.description}</td>
-          <td className="border-2 border-cyan-300 p-3">{t.dueDate}</td>
-          <td className="border-2 border-cyan-300 p-3">{t.status}</td>
+          <td className=" p-3">{t.name}</td>
+          <td className=" p-3">{t.description}</td>
+          <td className=" p-3">{t.dueDate}</td>
+          <td className=" p-3">{t.status}</td>
         </tr>
       ))}
     </tbody>

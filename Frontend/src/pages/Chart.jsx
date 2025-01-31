3e0c006c695844cc861fbd290478b9ccaba3280c
Prathmesh kulkarni <prathmeshkulkarni312@gmail.com>
2025-01-31 23:34:27 +0530

    mdd

--- Frontend/src/pages/Chart.jsx
+++ Frontend/src/pages/Chart.jsx
@@ -1,12 +1,11 @@
-
 import { Bar } from "react-chartjs-2";
+import { Chart as ChartJS, BarElement, CategoryScale, LinearScale, Tooltip, Legend } from "chart.js";
 
-import {Chart as ChartJS,BarElement,CategoryScale,LinearScale,Tooltip,Legend,} from "chart.js";
-
-
+// Register components for Chart.js
 ChartJS.register(BarElement, CategoryScale, LinearScale, Tooltip, Legend);
-function Chart(){
-const data = {
+
+function Chart() {
+  const data = {
     labels: ["Pending", "In-Progress", "Completed"],
     datasets: [
       {
@@ -16,15 +15,15 @@ const data = {
       },
     ],
   };
-  return(
+
+  return (
     <div className="bg-white mt-6 p-4 rounded-xl border-4 border-blue-400 shadow-lg w-full sm:w-11/12 md:w-2/3 lg:w-1/2 mx-auto">
-    <h2 className="text-xl md:text-2xl font-bold text-black text-center mb-4">
-      Task Overview
-    </h2>
-    <div className="w-full overflow-x-auto">
-      <Bar data={data} />
+      <h2 className="text-xl md:text-2xl font-bold text-black text-center mb-4">Task Overview</h2>
+      <div className="w-full overflow-x-auto">
+        <Bar data={data} />
+      </div>
     </div>
-  </div>
   );
 }
+
 export default Chart;

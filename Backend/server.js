482e2f33eb71007b64219e9d1f28aae33631d818
Prathmesh kulkarni <prathmeshkulkarni312@gmail.com>
2025-01-31 21:32:52 +0530

    Updated35s sdDashboarddsddT122r.jsx

--- Backend/server.js
+++ Backend/server.js
@@ -11,7 +11,7 @@ const app=express();
 app.use(cors({
     origin: [
       "http://localhost:5173",  // Local frontend
-      "task-management-system-mern-stack-e5tp.vercel.app" // Deployed frontend
+      // Deployed frontend
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],  // Allowed HTTP methods
     allowedHeaders: ["Content-Type", "Authorization"],  // Allowed headers

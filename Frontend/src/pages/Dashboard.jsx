19d85efc6d24ed41d978e9495ed7c7841ab86ca4
Prathmesh kulkarni <prathmeshkulkarni312@gmail.com>
2025-01-31 23:24:22 +0530

    md

--- Frontend/src/pages/Dashboard.jsx
+++ Frontend/src/pages/Dashboard.jsx
@@ -1,25 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import { BASE_URL } from "../config";
-import { Bar } from "react-chartjs-2";
-
-import {Chart as ChartJS,BarElement,CategoryScale,LinearScale,Tooltip,Legend,} from "chart.js";
-
-
-ChartJS.register(BarElement, CategoryScale, LinearScale, Tooltip, Legend);
-
-
+import Chart from "./Chart";
 const Dashboard = () => {
-  const data = {
-    labels: ["Pending", "In-Progress", "Completed"],
-    datasets: [
-      {
-        label: "Tasks",
-        data: [10, 5, 15], // Replace with actual counts
-        backgroundColor: ["#FF6384", "#FFCD56", "#36A2EB"],
-      },
-    ],
-  };
+  
 
   const location = useLocation();
   const username = location.state?.username || "User";
@@ -73,14 +57,7 @@ const Dashboard = () => {
       </div>
 
       {/* Chart Section */}
-      <div className="bg-white mt-6 p-4 rounded-xl border-4 border-blue-400 shadow-lg w-full sm:w-11/12 md:w-2/3 lg:w-1/2 mx-auto">
-        <h2 className="text-xl md:text-2xl font-bold text-black text-center mb-4">
-          Task Overview
-        </h2>
-        <div className="w-full overflow-x-auto">
-          <Bar data={data} />
-        </div>
-      </div>
+     <Chart/>     
 
       {/* Task Table */}
       <div className="bg-white mt-6 p-4 rounded-xl border-4 border-blue-400 shadow-lg w-full sm:w-11/12 md:w-5/6 mx-auto">
